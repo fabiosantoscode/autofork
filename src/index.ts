a5658bb3996316c0a996365082d75ffcf0cf1054
Fábio Santos <fabiosantosart@gmail.com>
2018-11-18 21:05:03 +0000

    .

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ const path = require('path')
 const { spawn } = require('child_process')
 const isUrl = require('is-url')
 const os = require('os')
+const rimraf = require('rimraf')
 const shellescape = require('shell-escape')
 
 const spawnPromise = (cmd: string, ...args: string[]) => spawnPromiseOptions({}, cmd, ...args)
@@ -21,32 +22,54 @@ const spawnPromiseOptions = (opt: any, cmd: string, ...args: string[]) => new Pr
   })
 })
 
-module.exports = async (
-  repo: string,
-  fork: string,
-  { diff }: { diff: string }
-) => {
-  if (isUrl(repo) || /[@:]/.test(repo)) {
+const isAUrl = (url: string) => isUrl(url) || /[@:]/.test(url)
+
+const possiblyFetch = async (repo: string) => {
+  if (isAUrl(repo)) {
     const repoTmp = await fs.mkdtemp(os.tmpdir() + '/')
     await spawnPromise('git', 'clone', repo, repoTmp)
-    repo = repoTmp
+    return repoTmp
   }
+  return repo
+}
+
+const possiblyRemove = async (repo: string, originalRepo: string) => {
+  if (isAUrl(originalRepo)) {
+    await new Promise(resolve => { rimraf(repo, resolve) })
+  }
+}
+
+module.exports = async (
+  originalRepo: string,
+  originalFork: string,
+  { diff }: { diff: string }
+) => {
+  const repo = await possiblyFetch(originalRepo)
+  const fork = await possiblyFetch(originalFork)
   if (!await fs.stat(fork).catch(() => null)) {
     await spawnPromise('cp', '-r', repo, fork)
   }
 
   const filename = path.join(fork, '.diff')
   await fs.writeFile(filename, diff)
-  const result = await spawnPromiseOptions({ cwd: fork }, 'git', 'apply', '.diff')
+  await spawnPromiseOptions({ cwd: fork }, 'git', 'apply', '.diff')
+  await spawnPromiseOptions({ cwd: fork }, 'git', 'commit', '-am', 'fork patch')
 
-  console.log(result)
+  if (isAUrl(originalFork)) {
+    await spawnPromiseOptions({ cwd: fork }, 'git', 'push')
+  }
+
+  possiblyRemove(repo, originalRepo)
+  possiblyRemove(fork, originalFork)
 }
 
 module.exports.maintain = async (
-  repo: string,
-  fork: string,
+  originalRepo: string,
+  originalFork: string,
   { diff }: { diff: string }
 ) => {
+  const repo = await possiblyFetch(originalRepo)
+  const fork = await possiblyFetch(originalFork)
   if (!await fs.stat(fork).catch(() => null)) {
     return module.exports(repo, fork, { diff })
   }
@@ -54,4 +77,11 @@ module.exports.maintain = async (
   await spawnPromiseOptions({ cwd: fork }, 'git', 'remote', 'add', 'foo', path.relative(fork, repo))
   await spawnPromiseOptions({ cwd: fork }, 'git', 'fetch', 'foo')
   await spawnPromiseOptions({ cwd: fork }, 'git', 'rebase', 'foo/master')
+
+  if (isAUrl(originalFork)) {
+    await spawnPromiseOptions({ cwd: fork }, 'git', 'push', '-f')
+  }
+
+  possiblyRemove(repo, originalRepo)
+  possiblyRemove(fork, originalFork)
 }
